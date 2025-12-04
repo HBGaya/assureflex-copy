41a4f363f1e11709e846c791d9d2b755e97b3cc1
HBGaya <hamzabilalgaya.bilal@gmail.com>
2025-12-05 04:50:46 +0500

    Base url changed....

--- lib/core/constants/env.dart
+++ lib/core/constants/env.dart
@@ -1,3 +1,4 @@
 class Env {
-  static const String baseUrl = 'https://api-assureflex.leensphere.com/api/v1/';
+  // static const String baseUrl = 'https://api-assureflex.leensphere.com/api/v1/';
+  static const String baseUrl = 'https://api-assureflex.leendesignstudio.com/';
 }
